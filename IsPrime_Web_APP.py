431f62bd62adefc3a36fe224f6f62b876efa619c
lokesh-saragadam <lokeshs2k6@gmail.com>
2024-09-23 16:07:17 +0300

    IsPrime_Web_APP.py

--- IsPrime_Web_APP.py
+++ IsPrime_Web_APP.py
@@ -14,12 +14,12 @@ selected = st.sidebar.selectbox(
 if (selected == 'assignment 0.1'):
     st.title('Assignment 0.1')
     def is_prime(n):
-    if n <= 1:
+     if n <= 1:
         return f"{n} is not a prime number."
-    for i in range(2, int(n**0.5) + 1):
+     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return f"{n} is not a prime number."
-    return f"{n} is a prime number." 
+     return f"{n} is a prime number." 
 
    def main():
      st.title("Check if a number is a prime number")
