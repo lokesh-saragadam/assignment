4916a0f8bb63e4cf3fbc7d6343950aecfc9686ef
lokesh-saragadam <lokeshs2k6@gmail.com>
2024-09-23 16:54:14 +0300

    IsPrime_Web_APP.py

--- IsPrime_Web_APP.py
+++ IsPrime_Web_APP.py
@@ -43,8 +43,8 @@ if (selected == 'assignment 1.0'):
       
 with tab2:
     st.title("About Us")
-    ourself = """We are
-                \n CH23BTECH11040-Saragadam Lokesh \n
+    ourself = """We are\n 
+                 CH23BTECH11040-Saragadam Lokesh \n
                  CH23BTECH11040-Vardan Gupta"""
     st.write(ourself)
 with tab3:
