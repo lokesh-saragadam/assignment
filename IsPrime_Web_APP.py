19f12f4cc99395ff961b436834261b58b5afd62c
lokesh-saragadam <lokeshs2k6@gmail.com>
2024-09-24 19:18:47 +0300

    IsPrime_Web_APP.py

--- IsPrime_Web_APP.py
+++ IsPrime_Web_APP.py
@@ -17,8 +17,7 @@ tab1, tab2, tab3 = st.tabs(["Home", "About", "Contact"])
 with tab1:
  st.title("Welcome to the Home Page")
  
- st.image("https://via.placeholder.com/150", caption="Example Image")
- if (selected == 'assignment 0.1'):
+   st.image("https://media.giphy.com/media/26xBwdIuRJiAIqHwA/giphy.gif", caption="Sample GIF") if (selected == 'assignment 0.1'):
    st.title('Assignment 0.1')
    def is_prime(n):
      if n <= 1:
